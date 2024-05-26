9759e4d7cdd6a65689f33bdd103f08d1bda34be0
Leonardo Pereda <leodpereda@outlook.com>
2024-05-25 20:06:11 -0400

    Completed the base of the app, now format to specific app use

--- Journal/pages/notebook.py
+++ Journal/pages/notebook.py
@@ -2,7 +2,8 @@
 
 import reflex as rx
 from reflex.style import toggle_color_mode
-from Journal.states import JournalData
+from Journal.components.form import render_post_form
+from Journal.states import Authentication, JournalData, Comments, Post
 from Journal.models import CustomPost
 
 def switch_theme_color():
@@ -24,9 +25,14 @@ def navbar():
                 rx.button("Menu"),
             ),
             rx.menu.content(
-                rx.menu.item("New Journal Entry"),
+                rx.menu.item(
+                    "New Journal Entry",
+                    on_click=Post.toggle_post_form),
                 rx.menu.separator(),
-                rx.menu.item("Logout"),
+                rx.menu.item(
+                    "Logout",
+                    on_click=Authentication.user_logout
+                    ),
             ),
         ),
         position="fixed",
@@ -38,21 +44,210 @@ def navbar():
         z_index="5",
     )
 
+def render_post_comment_form(item: CustomPost) -> list:
+    """ Renders the comment section of a post
+        With the option to comment on post and update on db
+    """
+    return [
+        rx.text(
+            "Comments",
+            font_size = "12px",
+            opacity="0.01",
+            display=item.is_comment_visible,
+        ),
+        rx.box(
+            rx.divider(opacity="0"),
+            padding="0.25em 10em",
+            display=item.is_comment_visible
+        ),
+        rx.text_area(
+            value=Comments.post_comment,
+            placeholder="Enter your comment here ...",
+            display=item.is_comment_visible,
+            on_change=Comments.update_post_comment,
+        ),
+        rx.box(
+            rx.divider(opacity="0"),
+            padding="0.25em 10em",
+            display=item.is_comment_visible
+        ),
+        rx.box(
+            rx.button(
+                "Comment",
+                display=item.is_comment_visible,
+                on_click=Comments.commit_comment_to_post(item),
+            ),
+            display='flex',
+            justify_content="end",
+            width="100%",
+        ),
+        rx.box(
+            rx.divider(opacity="0"),
+            padding="1em 10em",
+            display=item.is_comment_visible
+        ),
+    ]
+
+# The following renders the META DATA for each post -> timestamp, userName and toggle_comment
+def reusable_meta_data_component(_tag: str, _label: str, _name : str, fn: callable):
+    return rx.hstack(
+        rx.tooltip(
+            rx.icon(
+                tag=_tag,
+                transform="Scale(0.7)",
+                opacity="0.8",
+                cursor="pointer",
+                on_click=fn
+            ),
+            content=_label,
+            _dark={"bg":"#1b1b1b", "color":"white"},
+            _light={"bg":"#ffffff", "color":"black"},
+        ),
+        rx.text(
+            _name,
+            opacity="0.8",
+            font_size="10px"
+        ),
+        spacing="0",
+        display="flex",
+        align="center",
+        justify="start",
+    )
+
+# Next is to render the POST METADATA, similar to the method above
+def render_post_metadata(time: str, username: str, item: CustomPost):
+    return rx.hstack(
+        reusable_meta_data_component(
+            _tag="at_sign", _label="Username", _name=username,
+            fn=Comments.void_event
+        ),
+        reusable_meta_data_component(
+            _tag="clock", _label="Created On", _name=time,
+            fn=Comments.void_event
+        ),
+        rx.cond(
+            condition=item.is_comment_visible == "none",
+            c1=reusable_meta_data_component(
+                _tag="pencil",
+                _label="Comment",
+                _name="Leave a comment",
+                fn=Comments.toggle_comment(item)
+            ),
+            c2=reusable_meta_data_component(
+                _tag="circle-x",
+                _label="Comment",
+                _name="Close comment",
+                fn=Comments.toggle_comment(item),
+            ),
+        ),
+        spacing="2",
+    )
+
+# Finally have the component that renders the comments by other users
+def render_comments(item):
+    return rx.vstack(
+        rx.hstack(
+            rx.text(item.content),
+            width="100%",
+            display="flex",
+            justify_content="start",
+            align_items="center",
+        ),
+        rx.hstack(
+            reusable_meta_data_component(
+                _tag="at_sign", _label="Username", _name=item.username,
+                fn=Comments.void_event
+            ),
+            reusable_meta_data_component(
+                _tag="clock", _label="Created On", _name=item.created_at,
+                fn=Comments.void_event
+            ),
+            width="100%",
+            display="flex",
+            justify_content="start",
+            align_items="center",
+            spacing="2"
+        ),
+        width="100%",
+        display="flex",
+        padding_left="0.75rem",
+        _dark={"border_left":"1px solid gray"},
+        _light={"border_left":"1px solid black"}
+    )
+
 def render_post_header(item: str):
-    return rx.accordion.root(
-        rx.accordion.item(
-            header=item.title,
-            content=item.content
-            )
-        )
+    return rx.chakra.accordion_button(
+        rx.heading(item, size="2", text_align="start"),
+        rx.spacer(),
+        rx.chakra.accordion_icon()
+    )
+
+def render_post_content(data: str):
+    """
+        Render content of the post
+    """
+    return [
+        rx.text(
+            data,
+            text_align="start",
+            font_size={"13px","13px","14px","15px","15px",},
+            transition="all 550ms ease",
+        ),
+        rx.box(rx.divider(opacity="0"), padding="1em 0em")
+    ]
 
 def render_item(item: CustomPost):
-    print(item)
-    return rx.text(item.title)
+    return rx.chakra.accordion(
+        rx.chakra.accordion_item(
+            render_post_header(item.title),
+            rx.chakra.accordion_panel(
+                # * unpacks list of components
+                *render_post_content(item.content),
+                *render_post_comment_form(item),
+                rx.hstack(
+                    render_post_metadata(
+                        item.created_at,
+                        item.username,
+                        item
+                    ),
+                    width="100%",
+                    display="flex",
+                    justify="end",
+                    align="center"
+                ),
+                rx.divider(height="2", opacity="0"),
+                rx.vstack(
+                    # This iterator renders the comments
+                    rx.foreach(
+                        # NOTE: item.comments is a list[dict]
+                        iterable=item.comments,
+                        render_fn=render_comments
+                    ),
+                    width="100%",
+                    display="flex",
+                    justify="center",
+                    align="center",
+                    spacing="1"
+                ),
+            ),
+            padding="0.25em 0em",
+            margin="0",
+            overflow="hidden",
+            border="1px 0px 1px 0px solid gray"
+        ),
+        width=["100%", "100%", "80%", "65%", "60%"],
+        allow_multiple=True,
+        padding="0.65rem 0rem",
+        overflow="hidden",
+        reduce_motion=True,
+        transition="all 550ms ease"
+    )
 
 @rx.page("/notebook", on_load=JournalData.on_notebook_landing_event)
 def notebook():
-    return rx.fragment(
+    return rx.vstack(
+        #post form goes here
+        render_post_form(),
         navbar(),
         rx.vstack(
             rx.foreach(
@@ -61,6 +256,7 @@ def notebook():
             ),
 
             width="100%",
+            align="center",
             padding="5rem 1rem",
             overflow="auto",
             transition="all 550ms ease"
--- Journal/states.py
+++ Journal/states.py
@@ -1,6 +1,6 @@
 import reflex as rx
 from .models import CustomPost
-from .api import get_posts_with_comments_api, user_login_endpoint, user_registration_endpoint, is_user_authenticated, get_usernames
+from .api import get_posts_with_comments_api, insert_comment_to_database, insert_post_to_database, user_login_endpoint, user_logout_endpoint, user_registration_endpoint, is_user_authenticated, get_usernames
 
 class State(rx.State):
     def void_event(self): ...
@@ -79,6 +79,16 @@ class Authentication(LoginState):
         await self.get_username_list()
         return rx.redirect("/notebook")
 
+    async def user_logout(self):
+        response = await user_logout_endpoint(self.access_token)
+
+        # perform any pre-logout clean up: ie clear cookies...
+        self.access_token = ""
+
+        if response == 204:
+            return rx.redirect("/")
+
+
 class JournalData(Authentication):
     """
     Method that handles on Journal landing, ie when user reaches the /notebook route
@@ -107,3 +117,98 @@ class JournalData(Authentication):
             self.access_token, self.username_list
         )
         # print(self.posts)
+
+class Comments(Authentication):
+    post_comment: str #object that handles user's comment...
+
+    async def void_event(self):...
+
+    async def update_post_comment(self, post_comment):
+        self.post_comment = post_comment
+
+    # method that changes comment visibility on event
+    async def set_comment_visibility_to_flex(self, post: dict):
+        post["is_comment_visible"] = (
+            "flex" if post["is_comment_visible"] == "none" else
+            "none"
+        )
+        #add a return statement
+        return post
+
+    # method to update comment form visibility...
+    def update_posts_with_comment_visibility(self, post_with_comment: dict):
+        for post in self.posts:
+            if post.id == post_with_comment["id"]:
+                post.is_comment_visible = post_with_comment["is_comment_visible"]
+            else:
+                post.is_comment_visible = "none"
+        self.post_comment = ""
+
+    # toggle method for the comment
+    async def toggle_comment(self, post):
+        _post: dict = await self.set_comment_visibility_to_flex(post)
+        self.update_posts_with_comment_visibility(_post)
+
+    async def commit_comment_to_post(self, post: CustomPost):
+        data = {
+            "user_id": self.user_id,
+            "post_id": post["id"],
+            "content": self.post_comment
+        }
+
+        res = await insert_comment_to_database(
+            access_token=self.access_token,
+            comment=data
+        )
+
+        await self.toggle_comment(post)
+
+        if res == 201:
+            self.posts = await get_posts_with_comments_api(
+                self.access_token, self.username_list
+            )
+            return rx.redirect("/notebook")
+
+class Post(Authentication):
+    post_title: str
+    post_body: str
+
+    is_open: bool = False
+
+    async def clear_post_entries(self):
+        self.post_title, self.post_body = "", ""
+
+    def toggle_post_form(self):
+        self.is_open = not (self.is_open)
+
+    def update_post_title(self, post_title):
+        self.post_title = post_title
+
+    def update_post_body(self, post_body):
+        self.post_body = post_body
+
+    # method to insert post to supabase DB
+    async def submit_post_for_validation(self):
+        #make any pre-validation checks here
+        if await is_user_authenticated(self.access_token) is True:
+            response = await insert_post_to_database(
+                access_token=self.access_token,
+                user_id=self.user_id,
+                post_title=self.post_title,
+                post_body=self.post_body
+            )
+
+            if response == 200 or response == 201:
+                self.toggle_post_form()
+
+                # update post data
+                self.posts = await get_posts_with_comments_api(
+                    access_token=self.access_token,
+                    username_list=self.username_list
+                )
+
+                await self.clear_post_entries()
+
+                return rx.redirect("/notebook")
+        else:
+            print("Acess token not valid.")
